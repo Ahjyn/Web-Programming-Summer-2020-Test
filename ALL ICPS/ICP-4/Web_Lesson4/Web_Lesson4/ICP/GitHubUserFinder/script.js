875ca1b4ae5a921b991ab5a74edc60f29f59bd03
SangJin Hyun <54251062+Ahjyn@users.noreply.github.com>
2020-06-18 22:35:36 -0500

    ICP-4 Commit final

--- ALL ICPS/ICP-4/Web_Lesson4/Web_Lesson4/ICP/GitHubUserFinder/script.js
+++ ALL ICPS/ICP-4/Web_Lesson4/Web_Lesson4/ICP/GitHubUserFinder/script.js
@@ -1,33 +1,51 @@
 function getGithubInfo(user) {
     //1. Create an instance of XMLHttpRequest class and send a GET request using it.
     // The function should finally return the object(it now contains the response!)
+    var xhttp = new XMLHttpRequest(); //create instant of request
+    xhttp.open('GET', "https://api.github.com/users/" + user, true);
+    xhttp.send();
+
+
+    xhttp.onreadystatechange = function() {
+        //condition statement operations
+
+            if(xhttp.readyState == 4 && xhttp.status == 200)
+            {
+                var text = JSON.parse(xhttp.responseText);
+
+                console.log(text); // pass parameter response
+                showUser(text);
+            }
+            else {
+                noSuchUser(username);
+            }
+
+
+    };
 }
 
 function showUser(user) {
-    //2. set the contents of the h2 and the two div elements in the div '#profile' with the user content
+    $("#link").append("<a href='"+user.html_url+"'>Link URL</a>");
+    $("#profilePic").html("<img height='100' width='100' src='"+ user.avatar_url+"'/>");
+    $("#name").text('Name of user : '+user.login);
+    $("#Id").text('ID: '+user.id);
+
+
 }
 
 function noSuchUser(username) {
-    //3. set the elements such that a suitable message is displayed
+    alert("The user " + username + " is not available"); //concatenation of needed elements
 }
 
 $(document).ready(function () {
     $(document).on('keypress', '#username', function (e) {
-        //check if the enter(i.e return) key is pressed
         if (e.which == 13) {
-            //get what the user enters
-            username = $(this).val();
+            username = $(this).val(); // recalling this specific instance
             //reset the text typed in the input
             $(this).val("");
             //get the user's information and store the respsonse
-            response = getGithubInfo(username);
-            //if the response is successful show the user's details
-            if (response.status == 200) {
-                showUser(JSON.parse(response.responseText));
-                //else display suitable message
-            } else {
-                noSuchUser(username);
-            }
+            text = getGithubInfo(username);
+
         }
     })
 });
